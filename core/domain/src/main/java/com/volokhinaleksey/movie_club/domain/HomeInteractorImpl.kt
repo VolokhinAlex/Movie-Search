ca05a6ed8b26b69280b53cc1e0d20622eebf5689
volokhinaleksey <alexey@pingocean.com>
2024-11-14 18:58:06 +0500

    Фикс формата даты

--- core/domain/src/main/java/com/volokhinaleksey/movie_club/domain/HomeInteractorImpl.kt
+++ core/domain/src/main/java/com/volokhinaleksey/movie_club/domain/HomeInteractorImpl.kt
@@ -1,12 +1,16 @@
 package com.volokhinaleksey.movie_club.domain
 
 import com.volokhinaleksey.movie_club.data.repository.HomeRepository
+import kotlinx.coroutines.flow.map
 
 class HomeInteractorImpl(
     private val homeRepository: HomeRepository,
 ) : HomeInteractor {
 
     override fun getMovies(categoryId: String) = homeRepository.getMovies(categoryId)
+        .map {
+            it.map { it.copy(releaseDate = convertStringFullDateToOnlyYear(it.releaseDate)) }
+        }
 
     override suspend fun syncData(categoryId: String, lang: String) {
         try {
